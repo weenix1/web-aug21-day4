e1d27ce40105d772e29d82cb74d272474502df01
Weenix Star <60514231+weenix1@users.noreply.github.com>
2021-10-01 11:36:42 +0200

    new commit

--- day4/src/App.js
+++ day4/src/App.js
@@ -1,14 +1,15 @@
 import logo from "./logo.svg";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+
+import Gallery from "./components/Gallery";
 import MyNavBar from "./components/MyNavBar";
 
 function App() {
   return (
     <div className="App">
       <MyNavBar />
-      {/*   <Gallery />
-      <MyFooter /> */}
+      <Gallery />
     </div>
   );
 }
--- day4/src/components/MyNavBar.jsx
+++ day4/src/components/MyNavBar.jsx
@@ -1,68 +1,49 @@
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import { Button, Form, FormControl } from "react-bootstrap";
+
 const MyNavBar = () => (
-  <nav className="navbar navbar-expand-lg bg="dark" variant="dark" px-lg-5">
-    <a className="navbar-brand" href="#">
-      <img src="./img/Netflix_Logo_RGB.png" width={120} />
-    </a>
-    <button
-      className="navbar-toggler"
-      type="button"
-      data-toggle="collapse"
-      data-target="#navbarSupportedContent"
-      aria-controls="navbarSupportedContent"
-      aria-expanded="false"
-      aria-label="Toggle navigation"
-    >
-      <span className="navbar-toggler-icon" />
-    </button>
-    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul className="navbar-nav mr-auto">
-        <li className="nav-item active">
-          <a className="nav-link" href="#">
-            Home <span className="sr-only">(current)</span>
-          </a>
-        </li>
-        <li className="nav-item">Backoffice</li>
-      </ul>
-      <ul className="list-unstyled text-white">
-        <li className="d-inline-block align-middle mr-3">
-          <i className="bi bi-search" />
-        </li>
-        <li
-          className="d-inline-block align-middle mr-3"
-          style={{ opacity: "0.75", fontWeight: 600 }}
-        >
-          KIDS
-        </li>
-        <li className="d-inline-block align-middle mr-3">
-          <i className="bi bi-bell" />
-        </li>
-        <li className="d-inline-block align-middle">
-          <div className="btn-group dropleft">
-            <button
-              type="button"
-              className="btn text-white dropdown-toggle p-0"
-              data-toggle="dropdown"
-              aria-haspopup="true"
-              aria-expanded="false"
-            >
-              <i className="bi bi-person" style={{ fontSize: "1.3rem" }} />
-            </button>
-            <div className="dropdown-menu">
-              <a className="dropdown-item" href="#">
-                Action
-              </a>
-              <a className="dropdown-item" href="#">
-                Romance
-              </a>
-              <a className="dropdown-item" href="#">
-                Horror
-              </a>
-            </div>
-          </div>
-        </li>
-      </ul>
-    </div>
-  </nav>
+  <>
+    <Navbar bg="dark" variant="dark">
+      <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+      <Nav className="mr-auto">
+        <Nav.Link href="#home">Home</Nav.Link>
+        <Nav.Link href="#features">Features</Nav.Link>
+        <Nav.Link href="#pricing">Pricing</Nav.Link>
+      </Nav>
+      <Form inline>
+        <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+        <Button variant="outline-info">Search</Button>
+      </Form>
+    </Navbar>
+    <br />
+    <Navbar bg="primary" variant="dark">
+      <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+      <Nav className="mr-auto">
+        <Nav.Link href="#home">Home</Nav.Link>
+        <Nav.Link href="#features">Features</Nav.Link>
+        <Nav.Link href="#pricing">Pricing</Nav.Link>
+      </Nav>
+      <Form inline>
+        <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+        <Button variant="outline-light">Search</Button>
+      </Form>
+    </Navbar>
+
+    <br />
+    <Navbar bg="light" variant="light">
+      <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+      <Nav className="mr-auto">
+        <Nav.Link href="#home">Home</Nav.Link>
+        <Nav.Link href="#features">Features</Nav.Link>
+        <Nav.Link href="#pricing">Pricing</Nav.Link>
+      </Nav>
+      <Form inline>
+        <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+        <Button variant="outline-primary">Search</Button>
+      </Form>
+    </Navbar>
+  </>
 );
 
 export default MyNavBar;
